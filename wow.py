61ff470934072aaf56018f33ca13c56459fb2876
RaitE <raitegor@gmail.com>
2021-02-22 17:40:08 +0300

    1 задание

--- wow.py
+++ wow.py
@@ -1,3 +1,23 @@
-my_f = lambda a, b: a + b
+from requests import get, utils
 
-print(my_f)
+
+def currency_rates (valute):
+    response = get('http://www.cbr.ru/scripts/XML_daily.asp')
+    encodings = utils.get_encoding_from_headers(response.headers)
+    content = response.content.decode(encoding=encodings)
+    valute = valute.upper()
+    if valute in content and len(valute)==3:
+        a=content.split('</Valute>')
+        b = None
+        for i in a:
+            if valute in i:
+                b = i
+        end = b.find('</Value>')
+        start = end - 7
+        b = float(b[start:end].replace(',', '.'))
+        return b
+    else:
+        return None
+
+print (currency_rates('eur'))
+print (currency_rates('usd'))
