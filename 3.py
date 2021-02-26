c76917573ea6499684a4aca0a4da0266fa4b68d6
RaitE <raitegor@gmail.com>
2021-02-26 15:05:35 +0300

    3 done!

---
+++ 3.py
@@ -0,0 +1,14 @@
+import itertools
+
+tutors = [
+    'Иван', 'Анастасия', 'Петр', 'Сергей',
+    'Дмитрий', 'Борис', 'Елена'
+]
+klasses = [
+    '9А', '7В', '9Б', '9В', '8Б', '10А'
+]
+
+pupils = (i for i in itertools.zip_longest(tutors, klasses))
+
+print(type(pupils))
+print(*itertools.islice(pupils, 9))
