671b63dfc438e40c54271ca2e38728cc3d7fb17b
RaitE <raitegor@gmail.com>
2021-03-14 13:38:12 +0300

    1 done!

---
+++ 1.py
@@ -0,0 +1,13 @@
+
+numero = []
+with open('nginx_logs.txt', 'r', encoding='utf-8') as f:
+    black_list = ((line.split(' ')[0], line.split(' ')[5][1:], line.split(' ')[6]) for line in f)
+    for i in black_list:
+        numero.append(i)
+
+
+
+
+
+
+
