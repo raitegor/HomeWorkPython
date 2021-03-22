1160555b2739987fa042e7a73a4a74cd4e1b5b38
RaitE <raitegor@gmail.com>
2021-03-22 14:40:07 +0300

    done

--- wow.py
+++ wow.py
@@ -1,23 +1,41 @@
-from requests import get, utils
+class Transport:
+    # атрибуты класса
+    a_count = 0
+
+    # Конструктор
+    # атрибуты объекта
+    def __init__(self, n, m, y):
+        self.name = n
+        self.model = m
+        self.year = y
+        Auto.a_count += 1
+
+    # методы класса
+    def on_star(self, speed):
+        print(f'go-go car {self.name}, with spped {speed}')
+
+    def on_stop(self):
+        print('stop!')
+
+class Auto(Transport):
+    def drift(self, n, m, y, p):
+        super().__init__(n, m, y)
+        self.pass_ = p
+        print('Vg')
 
 
-def currency_rates (valute):
-    response = get('http://www.cbr.ru/scripts/XML_daily.asp')
-    encodings = utils.get_encoding_from_headers(response.headers)
-    content = response.content.decode(encoding=encodings)
-    valute = valute.upper()
-    if valute in content and len(valute)==3:
-        a=content.split('</Valute>')
-        b = None
-        for i in a:
-            if valute in i:
-                b = i
-        end = b.find('</Value>')
-        start = end - 7
-        b = float(b[start:end].replace(',', '.'))
-        return b
-    else:
-        return None
 
-print (currency_rates('eur'))
-print (currency_rates('usd'))
+auto_1 =Auto('mazda', 'cx-9', 2021)
+auto_1.on_star(35)
+auto_1.drift()
+
+
+# auto_1 = Auto('mazda', 'cx-9', 2021)
+# print(auto_1.name)
+# auto_1.on_star()
+# print(auto_1.a_count)
+# print()
+#
+# auto_2 = Auto('Lada', 'Niva', 2019)
+# print(auto_2.name)
+# print(auto_2.a_count)
